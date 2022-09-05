46bc1b94c2a1a6fe7b01afb4968f9a5a9520e48d
geofrancis <o.o.o.o@hotmail.co.uk>
2022-09-05 13:58:27 +0100

    Update radaradapter.ino

--- radaradapter.ino
+++ radaradapter.ino
@@ -1,5 +1,10 @@
 #include <Wire.h>
 #include "ld303-protocol.h"
+#include "Arduino.h"
+#include "editline.h"
+#include "cmdproc.h"
+
+
 
 //Standard MaxsonarI2CXL address
 #define I2C_SLAVE_ADDR  0x70  
@@ -11,58 +16,48 @@ static char cmdline[128];
 //number of readings to average
 const int numReadings = 10;
 
-static void printhex(const char *prefix, const uint8_t * buf, size_t len)
 
 
+
+int distances = 0;
+int distance = 0;
+unsigned char CS;
+uint8_t Index;
+byte received;
 int readings[numReadings]; 
 int readIndex = 0; 
 int total = 0;  
 int average = 0; 
-
+    uint8_t buf[32];
+    uint8_t data[16];
 
 void readradar(){     
      // process incoming data from radar
-    while (Serial.available()) {
-        uint8_t c = radar.read();
-          int len = protocol.get_data(buf);
-      uint8_t cmd = buf[0];
-         int dist = (buf[1] << 8) + buf[2];
-         int pres = buf[4];
-          n int k = (buf[5] << 8) + buf[6];
-        int micro = buf[7];
-          int off = buf[8];
+     while (Serial.available()) {
+        uint8_t c = Serial.read();
+
+        // run receive state machine
+        bool done = protocol.process_rx(c);
+        if (done) {
+            int len = protocol.get_data(buf);
+            uint8_t cmd = buf[0];
+            switch (cmd) {
+            case 0xD3:
+                int dist = (buf[1] << 8) + buf[2];
+                int pres = buf[4];
+                int k = (buf[5] << 8) + buf[6];
+                int micro = buf[7];
+                int off = buf[8];
+                printf("distance=%3d,present=%d,strength=%5d,micro=%d,off=%d\n", dist, pres, k, micro, off);
+                break;
         distances = 0.1 * dist;
             }
         }
     }
 }
 
-         
-// Smoothing
-{
-// subtract the last reading:       
-        total = total - readings[readIndex];
-        
-// read from the sensor:    
-         readings[readIndex] = distances ;
-         
-// add the reading to the total:
-         total = total + readings[readIndex];
-         
-// advance to the next position in the array:
-        readIndex = readIndex + 1;
-        
-// if we're at the end of the array...
-    if (readIndex >= numReadings) 
-    // ...wrap around to the beginning:
-    {readIndex = 0;
-    }
-// calculate the average:
-      average = total / numReadings;
-} 
-//ratio for speed of sound in air vs water 4.314 for water, 1.0 for use in air
-       distance = average * 4.314 ;    
-}
+     
+
 //look for i2c read read request
    void receiveEvent(int howMany) {
       while (Wire.available()) { // loop through all but the last
@@ -78,8 +73,8 @@ void readradar(){
 //send results over i2c
 void requestEvent() 
 {  
-Wire.write (highByte(distance));
-Wire.write (lowByte(distance));
+Wire.write (highByte(distances));
+Wire.write (lowByte(distances));
 }
 
 void setup() {
